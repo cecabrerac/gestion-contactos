1a5ae930d255e275b6f13ef63f1eea4f66e08229
cecabrerac <ccabrera9@hotmail.com>
2024-11-19 13:10:27 -0500

    script files

---
+++ frontend/scripts/navbarScript.js
@@ -0,0 +1,35 @@
+async function inicio() {
+  const contenido = document.getElementById("contenido");
+  const response = await fetch("./htmlFiles/inicio.html");
+  const data = await response.text();
+  contenido.innerHTML = data;
+}
+
+async function mostrar() {
+  const contenido = document.getElementById("contenido");
+  const response = await fetch("./htmlFiles/mostrar.html");
+  const data = await response.text();
+  contenido.innerHTML = data;
+  obtenerContactos();
+}
+
+async function agregar() {
+  const contenido = document.getElementById("contenido");
+  const response = await fetch("./htmlFiles/agregar.html");
+  const data = await response.text();
+  contenido.innerHTML = data;
+}
+
+async function actualizar() {
+  const contenido = document.getElementById("contenido");
+  const response = await fetch("./htmlFiles/actualizar.html");
+  const data = await response.text();
+  contenido.innerHTML = data;
+}
+
+async function borrar() {
+  const contenido = document.getElementById("contenido");
+  const response = await fetch("./htmlFiles/borrar.html");
+  const data = await response.text();
+  contenido.innerHTML = data;
+}
